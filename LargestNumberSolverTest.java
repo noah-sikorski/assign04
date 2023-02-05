044138d4115f955dd1ff1badfbb68ecf88e66d91
jaydenferrin <jayden.ferrin@gmail.com>
2023-02-04 19:38:12 -0700

    added test for readFile

--- LargestNumberSolverTest.java
+++ LargestNumberSolverTest.java
@@ -2,6 +2,8 @@ package assign04;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -74,6 +76,24 @@ class LargestNumberSolverTest
         tempList.add(tempArray2);
         assertArrayEquals(tempArray2, LargestNumberSolver.findKthLargest(tempList, 0));
         assertArrayEquals(tempArray1, LargestNumberSolver.findKthLargest(tempList, 1));
+    }
+
+    @Test
+    public void readFileNotExist() {
+        assertEquals(new ArrayList<Integer[]>(), LargestNumberSolver.readFile("skljdkljdsfkjlds"));
+    }
+
+    @Test
+    public void readFile() {
+//        File fil = new File("src/assign04/idk.txt");
+//        try {
+//            fil.createNewFile();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+        Integer[] expected = {410, 21, 93, 80, 69, 379, 20, 60, 432, 13, 72, 62, 70, 83, 9, 3, 14, 11, 62, 55, 34, 83, 80, 99, 56, 25, 79, 51, 51, 70, 79, 20, 34, 67, 40, 51, 41, 94, 89, 116, 874, 554, 137, 371, 17, 77, 97, 58, 83, 97, 26, 17, 54, 96, 33};
+        var actual = LargestNumberSolver.readFile("src/assign04/integers.txt");
+        assertArrayEquals(expected, actual.get(0));
     }
 
 
