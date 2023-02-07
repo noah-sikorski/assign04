bf7817525117953b4d775a1d93909f34e5000947
noah-sikorski <sikorskin1@gmail.com>
2023-02-06 20:45:18 -0700

    Added Tests

--- LargestNumberSolverTest.java
+++ LargestNumberSolverTest.java
@@ -1,9 +1,8 @@
 package assign04;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,57 +11,66 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LargestNumberSolverTest
 {
-    @Test
-    public void insertionSort()
+    Integer[] intArrSmall;
+    Integer[] intArrMedium;
+    Integer[] intArrLarge;
+    @BeforeEach
+    void setup()
     {
-        Integer[] actual = {54, 12, 346, 5, 87, 96, 5};
-        Integer[] expected = {5, 5, 12, 54, 87, 96, 346};
-        LargestNumberSolver.insertionSort(actual, Integer::compare);
-
-        assertArrayEquals(expected, actual);
+        intArrSmall = new Integer[]{37, 82, 42, 1};
+        intArrMedium = new Integer[]{54, 12, 346, 5, 87, 96, 5};
+        intArrLarge = new Integer[]{410, 21, 93, 80, 69, 379, 20, 60, 432, 13, 72, 62, 70, 83, 9, 3, 14, 11, 62, 55, 34, 83, 80, 99, 56, 25, 79, 51, 51, 70, 79, 20, 34, 67, 40, 51, 41, 94, 89, 116, 874, 554, 137, 371, 17, 77, 97, 58, 83, 97, 26, 17, 54, 96, 33};
     }
 
     @Test
-    public void digit() {
-        assertEquals(1, LargestNumberSolver.digit(130, 0));
-        assertEquals(3, LargestNumberSolver.digit(130, 1));
-        assertEquals(0, LargestNumberSolver.digit(130, 2));
-        assertEquals(1, LargestNumberSolver.digit(131, 87687));
-        assertEquals(8, LargestNumberSolver.digit(87691, 0));
-        assertEquals(9, LargestNumberSolver.digit(87691, 3));
+    public void insertionSort()
+    {
+        Integer[] expected = {5, 5, 12, 54, 87, 96, 346};
+        LargestNumberSolver.insertionSort(intArrMedium, Integer::compare);
+
+        assertArrayEquals(expected, intArrMedium);
     }
 
     @Test
     public void findLargestNumberTest()
     {
-        Integer[] tempArray = {1, 45, 9};
-        BigInteger expected = new BigInteger("9451");
-        assertEquals(expected, LargestNumberSolver.findLargestNumber(tempArray));
+        assertEquals(new BigInteger("8242371"), LargestNumberSolver.findLargestNumber(intArrSmall));
+        assertEquals(new BigInteger("9687555434612"), LargestNumberSolver.findLargestNumber(intArrMedium));
+    }
+
+
+    @Test
+    public void testFindLargestInt()
+    {
+        assertEquals(8242371 ,LargestNumberSolver.findLargestInt(intArrSmall));
     }
 
     @Test
-    public void findLargestIntTest()
+    public void testIntOutOfRangeException()
     {
-        Integer[] tempArray = {999, 639, 1, 7, 58, 9};
-        assertThrows(OutOfRangeException.class, () -> { LargestNumberSolver.findLargestInt(tempArray); });
+        assertThrows(OutOfRangeException.class, () -> { LargestNumberSolver.findLargestInt(intArrMedium); });
     }
 
     @Test
     public void findLargestLongTest()
     {
-        Integer[] tempArray = {999, 639, 1, 7, 58, 9};
-        assertEquals(99997639581L, LargestNumberSolver.findLargestLong(tempArray));
+        assertEquals(8242371L, LargestNumberSolver.findLargestLong(intArrSmall));
+        assertEquals(9687555434612L, LargestNumberSolver.findLargestLong(intArrMedium));
+    }
+
+    @Test
+    public void testLongOutOfRangeException()
+    {
+        assertThrows(OutOfRangeException.class, () -> { LargestNumberSolver.findLargestInt(intArrLarge); });
     }
 
     @Test
     public void sumTest()
     {
+        BigInteger expected = new BigInteger("9687563676983");
         List<Integer[]> tempList = new ArrayList<Integer[]>();
-        Integer[] tempArray = {88,51};
-        BigInteger expected = new BigInteger("106593");
-        tempList.add(tempArray);
-        tempArray = new Integer[]{7, 42, 97};
-        tempList.add(tempArray);
+        tempList.add(intArrSmall);
+        tempList.add(intArrMedium);
         assertEquals(expected, LargestNumberSolver.sum(tempList));
     }
 
@@ -91,9 +99,8 @@ class LargestNumberSolverTest
 //        } catch (IOException e) {
 //            throw new RuntimeException(e);
 //        }
-        Integer[] expected = {410, 21, 93, 80, 69, 379, 20, 60, 432, 13, 72, 62, 70, 83, 9, 3, 14, 11, 62, 55, 34, 83, 80, 99, 56, 25, 79, 51, 51, 70, 79, 20, 34, 67, 40, 51, 41, 94, 89, 116, 874, 554, 137, 371, 17, 77, 97, 58, 83, 97, 26, 17, 54, 96, 33};
         var actual = LargestNumberSolver.readFile("src/assign04/integers.txt");
-        assertArrayEquals(expected, actual.get(0));
+        assertArrayEquals(intArrLarge, actual.get(0));
     }
 
 
