c33deaf3d9215893d0c4c434cfddd807d3508b93
jaydenferrin <jayden.ferrin@gmail.com>
2023-02-09 16:10:37 -0700

    Change name of timer to title case

--- FindKthLargestTimer.java
+++ FindKthLargestTimer.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class findKthLargestTimer {
+public class FindKthLargestTimer {
 
     public static void main(String[] args) {
         Random rng = new Random();
